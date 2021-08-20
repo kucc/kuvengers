7a5a3a26f90c5bf21418b1f19c461d6aa4a6b281
keira-park <81363972+keira-park@users.noreply.github.com>
2021-08-20 23:51:29 +0900

    2nd version

--- tajaGame.js
+++ tajaGame.js
@@ -1,19 +1,51 @@
 // json 배열
 var jsonSource= [
-    {"name": "apple", "url": "fruits/apple.jpeg"},
+    /* {"name": "apple", "url": "fruits/apple.jpeg"},
     {"name": "banana", "url": "fruits/banana.png"},
     {"name": "cherry", "url": "fruits/cherry.png"},
     {"name": "grapes", "url": "fruits/grapes.png"},
     {"name": "kmelon", "url": "fruits/kmelon.png"},
     {"name": "orange", "url": "fruits/orange.png"},
-    {"name": "peach", "url": "fruits/peach.png"},
     {"name": "pear", "url": "fruits/pear.png"},
     {"name": "strawberry", "url": "fruits/strawberry.png"},
-    {"name": "watermelon", "url": "fruits/watermelon.png"},
+    {"name": "watermelon", "url": "fruits/watermelon.png"}, */
+    {"name": "강동원", "url": "images/강동원.jpg"},
+    {"name": "공유", "url": "images/공유.jpg"},
+    {"name": "김연경", "url": "images/김연경.jpg"},
+    {"name": "김연아", "url": "images/김연아.jpg"},
+    {"name": "김우빈", "url": "images/김우빈.jpg"},
+    {"name": "김태리", "url": "images/김태리.jpg"},
+    {"name": "남주혁", "url": "images/남주혁.jpg"},
+    {"name": "류준열", "url": "images/류준열.jpeg"},
+    {"name": "마동석", "url": "images/마동석.jpg"},
+    {"name": "바이든", "url": "images/바이든.jpg"},
+    {"name": "박보영", "url": "images/박보영.jpg"},
+    {"name": "박서준", "url": "images/박서준.jpg"},
+    {"name": "방시혁", "url": "images/방시혁.jpeg"},
+    {"name": "백석", "url": "images/백석.jpg"},
+    {"name": "백종원", "url": "images/백종원.jpg"},
+    {"name": "베컴", "url": "images/베컴.jpg"},
+    {"name": "봉준호", "url": "images/봉준호.jpeg"},
+    {"name": "비와이", "url": "images/비와이.jpeg"},
+    {"name": "뽀로로", "url": "images/뽀로로.jpg"},
+    {"name": "사나", "url": "images/사나.jpg"},
+    {"name": "서현진", "url": "images/서현진.jpg"},
+    {"name": "세종", "url": "images/세종.jpg"},
+    {"name": "손예진", "url": "images/손예진.jpg"},
+    {"name": "손흥민", "url": "images/손흥민.jpg"},
+    {"name": "송가인", "url": "images/송가인.jpg"},
 ]
+
+const total = jsonSource.length
+for (let j = 0; j < total; j++) {
+  jsonSource[j + total] = jsonSource[j]
+  jsonSource[j + total * 2] = jsonSource[j]
+}
+
 
 var jsonObjName = [];
 var jsonObjUrl = [];
+
 for (var i=0; i<jsonSource.length; i++) {
     jsonObjName.push(jsonSource[i][Object.keys(jsonSource[i])[0]]);
     jsonObjUrl.push(jsonSource[i][Object.keys(jsonSource[i])[1]]);
@@ -22,31 +54,27 @@ for (var i=0; i<jsonSource.length; i++) {
 // console.log(jsonObjUrl); // ["fruits/apple.jpeg", "fruits/banana.png", "fruits/cherry.png", "fruits/grapes.png", "fruits/kmelon.png", "fruits/orange.png", "fruits/peach.png", "fruits/pear.png", "fruits/strawberry.png", "fruits/watermelon.png"]
 
 var taja = jsonObjName;
+var tajaUrl = jsonObjUrl;
 
-// 밑에 선언한 tajaContents 안에 글자로 된 Div를 넣어주게 됩니다. 
+// 밑에 선언한 tajaContents 안에 Div를 넣기
 var tajaContents = document.getElementById("tajaContents");
 
 // tajaDiv를 다루기 위한 배열
 var newObj = [];
 
 // taja의 각 글자마다 top을 주기위한 배열 초기화 (글자를 내려오게 하기 위해 style 의 top을 이용할 것입니다)
-var plusTop = new Array(taja.length);
+var plusTop = new Array(jsonSource.length);
 for (let i = 0; i < plusTop.length; i++) {
 plusTop[i] = 0;
 }
 
 // 글자의 div 크기를 고정으로 주기 위한 변수입니다.
 const TAJAWIDTH = 150;
-const TAJAHEIGHT = 30;
+const TAJAHEIGHT = 200;
 
 // 그려지는 것 보다 내려오는게 간격이 더 짧게 함( setInterval의 시간 변수)
-const DRAWTIME = 1000;
+const DRAWTIME = 2500;
 const DOWNTIME = 500;
-
-// 생명 변수
-var life = 5;
-var lifeDiv = document.getElementById("life");
-lifeDiv.innerHTML = "LIFE : " + life;
 
 // 점수 변수
 var score = 0;
@@ -62,110 +90,86 @@ var randomPick = 0;
 var temp = null;
 
 // 랜덤으로 taja배열을 섞어주기 위한 for문
-for (let i = 0; i < taja.length; i++) {
-randomPick = Math.round(Math.random() * (taja.length - 1));
-temp = taja[randomPick];
+for (let i = 0; i < jsonSource.length; i++) {
+randomPick = Math.round(Math.random() * (jsonSource.length - 1));
+temp = tajaUrl[randomPick];
+tajaUrl[randomPick] = tajaUrl[i];
+tajaUrl[i] = temp;
+
+temp2 = taja[randomPick];
 taja[randomPick] = taja[i];
-taja[i] = temp;
+taja[i] = temp2;
 }
+
 
 // 일정한 간격으로 화면에 단어를 하나씩 뿌려주기 위한 setInteval 메서드 입니다.
 var drawInterval = setInterval(function () {
+    var leftWidth = Math.round(Math.random() * 1200);
+    var tajaDiv = document.createElement("div");
+    tajaDiv.classList.add("tajaWord");
 
-var leftWidth = Math.round(Math.random() * 1200);
-var tajaDiv = document.createElement("div");
-tajaDiv.classList.add("tajaWord");
-tajaDiv.style.width = TAJAWIDTH + "px";
-tajaDiv.style.height = TAJAHEIGHT + "px";
-tajaDiv.style.position = "absolute";
-tajaDiv.style.textAlign = "center";
-tajaDiv.innerHTML = taja[idx++];
-tajaContents.appendChild(tajaDiv);
-// leftWidth 변수가 0 < leftWidth < 1200 으로 설정되어있기 때문에
-// 글자의 width값 까지 더하게 되면 tajaContents의 범위를 넘어갈 수 있습니다.
-// 그래서 그 범위를 넘어가게 되면 안넘어가게 하기 위한 재설정해주는 부분입니다.
-if (leftWidth + TAJAWIDTH >= tajaContents.offsetWidth) {
-tajaDiv.style.left = (leftWidth - TAJAWIDTH) + "px";
-} else {
-tajaDiv.style.left = leftWidth + "px";
+    tajaDiv.id = 'taja[idx++]';
+    tajaDiv.innerHTML = '<img src="' + tajaUrl[idx++] + '" width="150" />';
+    tajaContents.appendChild(tajaDiv);
+
+    // css
+    tajaDiv.style.position = "absolute";
+    tajaDiv.style.justifyContent = "center";
+
+    // leftWidth 변수가 0 < leftWidth < 1200 으로 설정되어있기 때문에
+    // 글자의 width값 까지 더하게 되면 tajaContents의 범위를 넘어갈 수 있습니다.
+    // 그래서 그 범위를 넘어가게 되면 안넘어가게 하기 위한 재설정해주는 부분입니다.
+    if (leftWidth + TAJAWIDTH >= tajaContents.offsetWidth) {
+    tajaDiv.style.left = (leftWidth - TAJAWIDTH) + "px";
+    } else {
+    tajaDiv.style.left = leftWidth + "px";
+    }
+
+    // 각각의 tajaDiv를 다루기 위해 newObj 배열에 담는다.
+    newObj.push(tajaDiv);
+
+    // 화면에 글자가 다 뿌려지면 더 이상 글자를 뿌려주기 위한
+    // setInterval() 을 중지시킨다. ( 그렇지 않으면 글자가 다 뿌려진 이후에도 계속 동작하게 됩니다.)
+    if (newObj.length === jsonSource.length) {
+    clearInterval(drawInterval);
+    }
+    }, DRAWTIME);
 }
 
-// 각각의 tajaDiv를 다루기 위해 newObj 배열에 담는다.
-newObj.push(tajaDiv);
-
-// 화면에 글자가 다 뿌려지면 더 이상 글자를 뿌려주기 위한
-// setInterval() 을 중지시킨다. ( 그렇지 않으면 글자가 다 뿌려진 이후에도 계속 동작하게 됩니다.)
-if (newObj.length === taja.length) {
-clearInterval(drawInterval);
-}
-}, DRAWTIME);
-}
 // 글자를 내려주기 위한 메서드
 function downTaja() {
-setInterval(function () {
-for (let i = 0; i < taja.length; i++) {
-if (i < newObj.length) {
-    newObj[i].style.top = plusTop[i] + "px";
-    // 글자의 범위가 경계(바닥) 바깥으로 나갔을 경우 제거
-    if (plusTop[i] + TAJAHEIGHT >= tajaContents.offsetHeight) {
-        if (tajaContents.contains(newObj[i])) {
-            tajaContents.removeChild(newObj[i]);
-            life--;
-            lifeDiv.innerHTML = "LIFE : " + life;
-
-            // 목숨을 모두 잃었을 때 - 실패
-            if (life === 0) {
-                alert('5개의 생명을 모두 사용하셨습니다.');
-                alert('총 ' + score + '점을 획득하였습니다.');
-                location.reload();
-            }
-
-            // life가 남은상태로 게임이 끝났을 경우 - 성공
-            if (newObj.length === taja.length) { // 화면에 단어가 다 뿌려진 이후
-                if (!tajaContents.hasChildNodes()) { // 뿌려진 단어가 화면에 존재하지 않을 경우
-                    alert('다음 단계로 이동합니다.(준비중)');
-                    alert('총 ' + score + '점을 획득하였습니다.');
-                    location.reload();
+    setInterval(function () {
+        for (let i = 0; i < jsonSource.length; i++) {
+            if (i < newObj.length) {
+                newObj[i].style.top = plusTop[i] + "px";
+                // 글자의 범위가 경계(바닥) 바깥으로 나갔을 경우 제거
+                if (plusTop[i] + TAJAHEIGHT >= tajaContents.offsetHeight) {
+                    if (tajaContents.contains(newObj[i])) {
+                        tajaContents.removeChild(newObj[i]);
+                    }
                 }
+                plusTop[i] += 30;
             }
-
         }
-    }
-    plusTop[i] += 30;
-}
-}
-}, DOWNTIME);
-
+    }, DOWNTIME);
 }
 
 
 var tajaText = document.getElementById("tajaText");
 tajaText.addEventListener("keydown", function (e) {
-// enter 눌렀을 때
-if (e.keyCode === 13) {
-for (let i = 0; i < newObj.length; i++) {
-// 타자 친 단어와 화면의 단어가 일치했을 때
-if (tajaText.value === newObj[i].innerHTML) {
-    tajaContents.removeChild(newObj[i]);
-    score += 100;
-    scoreDiv.innerHTML = "SCORE : " + score;
-
-    // 더이상 화면에 뿌려질 단어가 없고
-    // life를 다 소진하지않고 clear 했을 경우
-    if (newObj.length === taja.length) {
-        if (!tajaContents.hasChildNodes()) {
-            alert('다음 단계로 이동합니다.(준비중)');
-            alert('총 ' + score + '점을 획득하였습니다.');
-            location.reload();
+    // enter 눌렀을 때
+    if (e.keyCode === 13) {
+        for (let i = 0; i < newObj.length; i++) {
+            // 타자 친 단어와 화면의 단어가 일치했을 때
+            if (tajaText.value === newObj[i].innerText) {
+                tajaContents.removeChild(newObj[i]);
+                score += 100;
+                scoreDiv.innerHTML = "SCORE : " + score;
+            }
         }
-
+        // enter 눌렀을 때 input 창 초기화
+        tajaText.value = "";
     }
-
-}
-}
-// enter 눌렀을 때 input 창 초기화
-tajaText.value = "";
-}
 });
 
 // 클릭 횟수에 대한 변수
