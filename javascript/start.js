02c05e2e2a58c1caa1d4c1eaeba0fef1c34cf3bd
mellykim123 <mellykim123@naver.com>
2021-08-20 23:53:26 +0900

    start page animation added

--- javascript/start.js
+++ javascript/start.js
@@ -1,1 +1,12 @@
-const startBtn = document.getElementById("startBtn")
+const userid = document.getElementById("username")
+const startBtn = document.getElementById("username")
+
+startBtn.addEventListener("click", (event) => {
+  if (userid.value === null) {
+    event.preventDefault()
+    alert("write a valid id")
+  }
+})
+if (userid.value === null) {
+  alert("write a valid id")
+}
