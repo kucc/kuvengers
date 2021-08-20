e1efe2c1fec293a5c6d1407d67c684efc65fe862
mellykim123 <mellykim123@naver.com>
2021-08-20 16:43:50 +0900

    timer

---
+++ javascript/timer.js
@@ -0,0 +1,18 @@
+let time = 90
+let timerId = null
+const startBtn = document.getElementById("tajaBtn")
+
+function timer(begin) {
+  if (timerId) return
+  timerId = setInterval(() => {
+    if (time <= 0) {
+      clearInterval(timerId)
+      return
+    }
+
+    time--
+    document.getElementById("remainingTime").innerHTML = "Remaining Time: " + time
+  }, 1000)
+}
+
+startBtn.addEventListener("click", timer)
